c42f032173132758e25a727fcdf6602fc20355fd
swati <01fe22bca002@kletech.ac.in>
2024-03-30 19:28:54 +0530

    calculator information

---
+++ ExtendedCalculator4.java
@@ -0,0 +1,12 @@
+public class ExtendedCalculator4 extends Calculator4 {
+    public float Square(float num)
+    {
+        return(num * num); 
+    }
+    public float Cube(float num)
+    {
+        return(num * num * num); 
+    }
+    
+}
+
